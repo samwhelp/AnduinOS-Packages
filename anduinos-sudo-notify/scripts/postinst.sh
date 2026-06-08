b7970fa822b67ca06915157d7c2cc168ff16eb9a
Anduin Xue <anduin@aiursoft.com>
2026-06-08 14:46:34 +0000

    fix: sudo-notify postinst and version

--- anduinos-sudo-notify/scripts/postinst.sh
+++ anduinos-sudo-notify/scripts/postinst.sh
@@ -8,7 +8,7 @@ if [ "$1" = "configure" ]; then
     if [ -f "$PAM_FILE" ]; then
         if ! grep -qF "$PAM_LINE" "$PAM_FILE"; then
             # Insert before the first @include line
-            sed -i "0,/^@include/{s/^@include/$PAM_LINE\n@include/}" "$PAM_FILE"
+            sed -i "0,/^@include/{s|^@include|$PAM_LINE\n@include|}" "$PAM_FILE"
         fi
     fi
 fi
